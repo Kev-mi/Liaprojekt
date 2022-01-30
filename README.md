b60e016d38a6edc4ff63239d82d2c1304cbe5d2d
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-01-30 19:36:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Liaprojekt
+ 
---
+++ train_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('dimensions.csv')
+
