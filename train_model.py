d486d6b91525376b54bfba4ee416dc6692fdafeb
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-03-13 19:45:07 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,8 +2,22 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('dimensions.csv')
 
+X = df[["Length", "Height", "Width"]]
+y = df["Price"]
+
+regr = linear_model.LinearRegression()
+regr.fit(X,y)
+
+def price_predictor(Length_par, Height_par, Width_par):
+    Predicted_price_function = regr.predict([[Length_par, Height_par, Width_par]])
+    return Predicted_price_function
+
+
+Length,Height, Width = input("Enter Length, Height and Width in that order to predict price")
+Predicted_price = Price_predictor(Length,Height, Width)
