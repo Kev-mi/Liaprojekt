585fc83b973776f42ffe5637c5aa7786283c2831
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-04-27 19:11:20 +0200

    Update Liaproject.py

--- Liaproject.py
+++ Liaproject.py
@@ -7,6 +7,7 @@ import plotly.express as px
 from scipy.stats import pearsonr
 import datetime as dt
 import numpy as np
+import math
 
 
 def df_filter_function(df_f, Length_filter, Height_filter, Width_filter, Price_filter, year_filter):
@@ -34,10 +35,10 @@ def csv_append(file_name, list_of_elem):
 
 def append_menu():
     with st.form("my_form"):
-        st.write("Fan info")
-        Width = st.text_input("Width")
-        Height = st.text_input("Height")
-        Length = st.text_input("Length")
+        st.write("Building info")
+        Width = st.text_input("Building Width")
+        Height = st.text_input("Building Height")
+        Length = st.text_input("Building Length")
         Price = st.text_input("Price")
         Date = st.text_input("Input date if not today (yyyy-mm-dd)")
         submitted = st.form_submit_button("Submit")
@@ -50,26 +51,28 @@ def append_menu():
 
 def predict_menu(df):
     with st.form("my_form"):
-        st.write("Fan info")
+        st.write("Building info")
         train_year = st.sidebar.selectbox("Select year to use data from", sorted(set(pd.DatetimeIndex(df['Date']).year)))
         train_month = st.sidebar.selectbox("Select month to use data from",sorted(set(pd.DatetimeIndex(df['Date']).month)))
-        df_year = df[pd.DatetimeIndex(df['Date']).year == train_year]
-        df_month = df[pd.DatetimeIndex(df['Date']).month == train_month]
-        df_pred = df_year[(df_year.isin(df_month))].dropna()
-        Width_pred = st.text_input("Width")
-        Height_pred = st.text_input("Height")
-        Length_pred = st.text_input("Length")
+        Width_pred = st.text_input("Building Width")
+        Height_pred = st.text_input("Building Height")
+        Length_pred = st.text_input("Building Length")
         submitted = st.form_submit_button("Predict price")
         font_size = st.sidebar.slider("Enter text size", 10, 100, value=25)
         if submitted:
+            testing = pd.to_datetime((str(train_year) + "-" + str(train_month) + "-" + "01")).date()
+            for x in range(0, len((df['Date']))):
+                df['Date'][x] = pd.to_datetime(df['Date'][x])
+            df_pred = df.loc[((df['Date'] > testing)).dropna()]
             X = df_pred[['Width', 'Height', 'Length']]
             y = df_pred['Price']
             regr = linear_model.LinearRegression()
             regr.fit(X, y)
             predicted_price = regr.predict([[Width_pred, Height_pred, Length_pred]])
-            string_output = "price is " + str(predicted_price[0]) + "kr"
+            string_output = "price is " + str(math.floor(predicted_price[0])) + "tkr" + " (exklusive resekostnader)"
             html_str = f"""<style>p.a{{font:bold {font_size}px Courier;}}</style><p class="a">{string_output}</p> """
             st.markdown(html_str, unsafe_allow_html=True)
+            st.write(str(int(Height_pred)*3)+ "% besparing")
 
 
 def result_menu(df):
@@ -83,7 +86,7 @@ def result_menu(df):
     for x in df.index:
         year_list.append(str(df["Date"][x])[:4])
     year_list = set(year_list)
-    selected_year = st.sidebar.selectbox("Select year", year_list)
+    selected_year = st.sidebar.selectbox("Select year", sorted(year_list))
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
     df = df_filter_function(df, Length_slider, Height_slider, Width_slider, Price_slider, selected_year)
     st.write(df)
