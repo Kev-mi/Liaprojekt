c7969406ba6d934213ef9a902ea0cc23ee0323bf
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-05-03 12:48:32 +0200

    Update Liaproject.py

--- Liaproject.py
+++ Liaproject.py
@@ -33,6 +33,7 @@ def duplicate_remover(df_duplicate):
 def csv_append(file_name, list_of_elem):
     with open(file_name, 'a+', lineterminator='\n') as write_obj:
         csv_writer = writer(write_obj)
+        csv_writer.writerow("")
         csv_writer.writerow(list_of_elem)
 
 
