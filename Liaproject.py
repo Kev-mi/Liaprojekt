3d6e8169dc080be67af07d269ad2edea2171d7a7
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-05-03 13:35:00 +0200

    Update Liaproject.py

--- Liaproject.py
+++ Liaproject.py
@@ -55,7 +55,7 @@ def append_menu(local_csv):
             local_csv = csv_append('Train.csv', row_contents)
     st.write(local_csv)
     try:
-        local_csv = local_csv.id.apply(str)
+        local_csv = local_csv.apply(str)
         st.download_button('Download CSV', local_csv, 'train.csv')
         with open('myfile.csv') as f:
             st.download_button('Download CSV', f)
