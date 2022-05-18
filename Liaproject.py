b962e3f7e74c081f2d75347aa55b29975a449533
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-05-18 16:39:21 +0200

    Update Liaproject.py

--- Liaproject.py
+++ Liaproject.py
@@ -22,7 +22,7 @@ def distance_calc(start_city, end_city):
     coordinates = {"Malmö" : (55.60587, 13.00073), "Göteborg" : (57.708870, 11.974560), "Stockholm" : (59.334591, 18.063240) }
     distance = geodesic(coordinates[start_city], coordinates[end_city]).km
     st.write("distance from " + start_city + " to " + end_city + " is " + str(round(distance, 2)) + "km")
-    df = pd.DataFrame({'lat': [coordinates[start_city(0)], coordinates[end_city(0)]], 'lon': [coordinates[start_city(1)], coordinates[end_city(1)]]})
+    df = pd.DataFrame({'lat': [coordinates[start_city], coordinates[end_city]], 'lon': [coordinates[start_city], coordinates[end_city]]})
     st.write(df)
 
 
